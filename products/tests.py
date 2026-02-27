582103e037889d338ad0a37104107efac2626644
JstLevi <levibaron143@gmail.com>
2026-02-27 20:41:14 +0800

    STEVE: add Swagger docs + automated POST test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
 
-# Create your tests here.
+class ProductAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_product(self):
+        url = reverse("product-list", kwargs={"version": "v1"})
+        payload = {"name": "Sample", "price": "10.00", "stock": 3}
+        response = self.client.post(url, payload, format="json")
+        self.assertEqual(response.status_code, 201)
