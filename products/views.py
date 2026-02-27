4a4d659d746ebcf18fde4cf466d2aeba11bf2dd0
JstLevi <levibaron143@gmail.com>
2026-02-27 20:38:33 +0800

    JOSH: implement versioned CRUD API at /api/v1/products/

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all().order_by("-id")
+    serializer_class = ProductSerializer
